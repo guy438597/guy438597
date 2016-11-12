7b77454c266f554d31d7e674648606f1af5a3032
Burny <b.riebe@gmx.de>
2016-11-12 13:00:59 +0100

    asdd

--- creeproles.js
+++ creeproles.js
@@ -1,4 +1,4 @@
-var calculations, chooseClosest, findEnergy, getDistance, getDistanceInTicks;
+var calculations, chooseClosest, findEnergy, getDistance, getDistanceInTicks, harvester, loadDefaultValues;
 
 calculations = require("./calculations");
 
@@ -10,50 +10,49 @@ getDistance = calculations.getDistance;
 
 getDistanceInTicks = calculations.getDistanceInTicks;
 
-({
-  loadDefaultValues: function(creep) {
-    if (!creep.memory.retreatRoomName) {
-      return creep.memory.retreatRoomName = Game.spawns.Spawn1.room.name;
+loadDefaultValues = function(creep) {
+  if (!creep.memory.retreatRoomName) {
+    return creep.memory.retreatRoomName = Game.spawns.Spawn1.room.name;
+  }
+};
+
+harvester = function(creep) {
+  var target;
+  loadDefaultValues(creep);
+  if (!creep.memory.state) {
+    creep.memory.state = "mining";
+  }
+  creep.memory.state = creep.carry.energy === creep.carryCapacity ? "deliverEnergy" : void 0;
+  creep.memory.state = creep.carry.energy === 0 ? "mining" : void 0;
+  if (creep.memory.target) {
+    target = Game.getObjectById(creep.memory.target);
+  }
+  if (creep.memory.state === "mining") {
+    if (!target) {
+      target = findNearbyDroppedEnergy(creep, 5);
     }
-  },
-  harvester: function(creep) {
-    var target;
-    loadDefaultValues(creep);
-    if (!creep.memory.state) {
-      creep.memory.state = "mining";
+    if (target && target.amount) {
+      pickupEnergy(creep, target);
     }
-    creep.memory.state = creep.carry.energy === creep.carryCapacity ? "deliverEnergy" : void 0;
-    creep.memory.state = creep.carry.energy === 0 ? "mining" : void 0;
-    if (creep.memory.target) {
-      target = Game.getObjectById(creep.memory.target);
+    if (!target) {
+      target = findMiningSite(creep);
     }
-    if (creep.memory.state === "mining") {
-      if (!target) {
-        target = findNearbyDroppedEnergy(creep, 5);
-      }
-      if (target && target.amount) {
-        pickupEnergy(creep, target);
-      }
-      if (!target) {
-        target = findMiningSite(creep);
-      }
-      if (target && !target.amount) {
-        return goMine(creep, target);
-      }
-    } else if (creep.memory.state === "deliverEnergy") {
-      if (!target) {
-        target = creep.room.find(FIND_STRUCTURES, {
-          filter: function(s) {
-            return s.structureType === STRUCTURE_SPAWN;
-          }
-        });
-      }
-      if (target) {
-        return goTransferEnergy(creep, target);
-      }
+    if (target && !target.amount) {
+      return goMine(creep, target);
+    }
+  } else if (creep.memory.state === "deliverEnergy") {
+    if (!target) {
+      target = creep.room.find(FIND_STRUCTURES, {
+        filter: function(s) {
+          return s.structureType === STRUCTURE_SPAWN;
+        }
+      });
+    }
+    if (target) {
+      return goTransferEnergy(creep, target);
     }
   }
-});
+};
 
 module.exports.loadDefaultValues = loadDefaultValues;
 
